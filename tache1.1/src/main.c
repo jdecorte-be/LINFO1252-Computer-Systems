6c6722045ea57de935b8166b13d5f7ae001c34b5
John Decorte <john.decorte01@gmail.com>
2024-11-13 11:52:01 +0100

    Base sys info

---
+++ tache1.1/src/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+
+
+
+int main(int ac, char **av)
+{
+    if (ac != 2)
+    {
+        printf("Usage: %s <number_of_philosophers>\n\n", av[0]);
+        return (1);
+    }
+
+}
